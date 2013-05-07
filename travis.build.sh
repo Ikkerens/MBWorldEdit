393fe06e371d30ee5ac3961f45deb8ba2ab277de
Ikkerens <ikkerens@hotmail.com>
2013-05-07 15:11:59 +0200

    Unzip fail

--- travis.build.sh
+++ travis.build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 wget https://dl.dropboxusercontent.com/u/55284851/MineBuilder/MBServerApi-beta.zip
-unzip -j MBServerApi-beta.zip MBServerAPI.jar -d MBServerApi.jar
+unzip -j MBServerApi-beta.zip MBServerAPI.jar
 find src/ 2>&1 | grep .java > sources.txt
 mkdir classes
 javac -cp ./MBServerAPI.jar:. -d classes -sourcepath src @sources.txt
