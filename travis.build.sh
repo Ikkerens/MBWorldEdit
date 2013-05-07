82de9aaeb880016a5b0d0f8a0d7ea1323c88ddd0
Ikkerens <ikkerens@hotmail.com>
2013-05-07 14:59:23 +0200

    Ignoring unzip verbose data

--- travis.build.sh
+++ travis.build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 wget https://dl.dropboxusercontent.com/u/55284851/MineBuilder/MBServerApi-beta.zip
-unzip -j "MBServerApi-beta.zip" "MBServerApi.jar" -d "MBServerApi.jar"
+unzip MBServerApi-beta.zip 2>&1 >/dev/null
 find src/ 2>&1 | grep .java > sources.txt
 javac -classpath MBServerApi.jar -sourcepath src @sources.txt
