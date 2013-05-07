eb5fb48e0c7be8514d548e8b5af26e605a0acf60
Ikkerens <ikkerens@hotmail.com>
2013-05-07 15:01:47 +0200

    Classpath derp

--- travis.build.sh
+++ travis.build.sh
@@ -3,4 +3,4 @@
 wget https://dl.dropboxusercontent.com/u/55284851/MineBuilder/MBServerApi-beta.zip
 unzip MBServerApi-beta.zip 2>&1 >/dev/null
 find src/ 2>&1 | grep .java > sources.txt
-javac -classpath MBServerApi.jar -sourcepath src @sources.txt
+javac -cp ./MBServerApi.jar:. -sourcepath src @sources.txt
