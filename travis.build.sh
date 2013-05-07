bace7b2c4ec04574295f70ec412913ed4f5eb495
Ikkerens <ikkerens@hotmail.com>
2013-05-07 15:10:05 +0200

    Fixed travis build

--- travis.build.sh
+++ travis.build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 wget https://dl.dropboxusercontent.com/u/55284851/MineBuilder/MBServerApi-beta.zip
-unzip MBServerApi-beta.zip 2>&1 >/dev/null
+unzip -j MBServerApi-beta.zip MBServerAPI.jar -d MBServerApi.jar
 find src/ 2>&1 | grep .java > sources.txt
-javac -cp ./MBServerApi.jar:. -sourcepath src @sources.txt
+mkdir classes
+javac -cp ./MBServerAPI.jar:. -d classes -sourcepath src @sources.txt
