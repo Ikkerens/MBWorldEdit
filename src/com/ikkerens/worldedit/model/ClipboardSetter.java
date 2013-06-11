4f31d984f4aaa1accaa0065ed7a506ed7979c956
ikkerens <ikkerens@hotmail.com>
2013-06-11 15:15:09 +0200

    Refactoring

---
+++ src/com/ikkerens/worldedit/model/ClipboardSetter.java
@@ -0,0 +1,19 @@
+package com.ikkerens.worldedit.model;
+
+class ClipboardSetter extends SetBlockType {
+    private final short[][][] clipboard;
+
+    ClipboardSetter( final Clipboard clipboard ) {
+        this.clipboard = clipboard.getBlocks();
+    }
+
+    @Override
+    public short getNextBlock( final int x, final int y, final int z ) {
+        final int xp = Math.abs( x ) % this.clipboard.length;
+        final int yp = Math.abs( y ) % this.clipboard[ 0 ].length;
+        final int zp = Math.abs( z ) % this.clipboard[ 0 ][ 0 ].length;
+
+        return this.clipboard[ xp ][ yp ][ zp ];
+    }
+
+}
