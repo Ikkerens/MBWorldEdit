db8f5d7a1f768857dc7ea179b68218d265f45fdc
Ikkerens <ikkerens@hotmail.com>
2013-11-08 14:55:13 +0100

    Added argument to undo, you can now use //undo [repeat].

--- src/com/ikkerens/worldedit/commands/UndoCommand.java
+++ src/com/ikkerens/worldedit/commands/UndoCommand.java
@@ -13,10 +13,23 @@ public class UndoCommand extends ActionCommand< WorldEditPlugin > {
 
     @Override
     protected void execute( final String label, final Player player, final String[] args ) {
-        if ( this.getSession( player ).undoLast() )
-            player.sendMessage( "Undone last action." );
-        else
-            player.sendMessage( "Action history is empty." );
+        int repeat = 1;
+        if ( args.length > 0 ) {
+            try {
+                repeat = Integer.parseInt( args[ 1 ] );
+            } catch ( NumberFormatException e ) {
+                player.sendMessage( String.format( "%s is not a valid number.", args[ 1 ] ) );
+            }
+        }
+
+        for ( int i = 0; i < repeat; i++ ) {
+            if ( this.getSession( player ).undoLast() )
+                player.sendMessage( "Undone last action." );
+            else {
+                player.sendMessage( "Action history is empty." );
+                break;
+            }
+        }
     }
 
 }
