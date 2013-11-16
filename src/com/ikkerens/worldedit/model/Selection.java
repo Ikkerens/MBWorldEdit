a7096e87f584e36c38cf74f4e95858688c234755
Ikkerens <ikkerens@hotmail.com>
2013-11-16 22:33:15 +0100

    Added support for 1.12 line projection

--- src/com/ikkerens/worldedit/model/Selection.java
+++ src/com/ikkerens/worldedit/model/Selection.java
@@ -1,7 +1,9 @@
 package com.ikkerens.worldedit.model;
 
 import com.mbserver.api.Constructors;
+import com.mbserver.api.dynamic.UILine;
 import com.mbserver.api.game.Location;
+import com.mbserver.api.game.Player;
 import com.mbserver.api.game.World;
 
 public class Selection {
@@ -83,6 +85,44 @@ public class Selection {
         final String pos2text = this.posText( this.pos2 );
 
         this.session.getPlayer().sendMessage( String.format( "Selection: %s to %s (Count: %s)", pos1text, pos2text, this.getCount() ) );
+
+        // Build lines
+        this.session.getPlayer().clearLines();
+        if ( this.isValid() ) {
+            final Location pos1 = this.getMinimumPosition();
+            final Location pos2 = this.getMaximumPosition().add( 1, 1, 1 );
+            final World world = pos1.getWorld();
+            final Player player = this.session.getPlayer();
+
+            final Location pos3, pos4, pos5, pos6, pos7, pos8;
+            // Bottom positions
+            pos3 = Constructors.newLocation( world, pos2.getBlockX(), pos1.getBlockY(), pos1.getBlockZ() );
+            pos4 = Constructors.newLocation( world, pos1.getBlockX(), pos1.getBlockY(), pos2.getBlockZ() );
+            pos5 = Constructors.newLocation( world, pos2.getBlockX(), pos1.getBlockY(), pos2.getBlockZ() );
+
+            // Upper positions
+            pos6 = Constructors.newLocation( world, pos1.getBlockX(), pos2.getBlockY(), pos2.getBlockZ() );
+            pos7 = Constructors.newLocation( world, pos2.getBlockX(), pos2.getBlockY(), pos1.getBlockZ() );
+            pos8 = Constructors.newLocation( world, pos1.getBlockX(), pos2.getBlockY(), pos1.getBlockZ() );
+
+            // Bottom lines
+            player.drawLine( new UILine( pos1, pos3 ), false );
+            player.drawLine( new UILine( pos1, pos4 ), false );
+            player.drawLine( new UILine( pos3, pos5 ), false );
+            player.drawLine( new UILine( pos4, pos5 ), false );
+
+            // Upper lines
+            player.drawLine( new UILine( pos2, pos6 ), false );
+            player.drawLine( new UILine( pos2, pos7 ), false );
+            player.drawLine( new UILine( pos6, pos8 ), false );
+            player.drawLine( new UILine( pos7, pos8 ), false );
+
+            // Side lines
+            player.drawLine( new UILine( pos1, pos8 ), false );
+            player.drawLine( new UILine( pos3, pos7 ), false );
+            player.drawLine( new UILine( pos4, pos6 ), false );
+            player.drawLine( new UILine( pos5, pos2 ), true );
+        }
     }
 
     private String posText( final Location pos ) {
