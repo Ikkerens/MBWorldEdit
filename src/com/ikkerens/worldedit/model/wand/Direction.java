19669871961d5d362a838e1bc9b5bc30c1f66414
ikkerens <ikkerens@space-walrus.com>
2014-11-19 14:31:45 +0100

    Reversed east/west

--- src/com/ikkerens/worldedit/model/wand/Direction.java
+++ src/com/ikkerens/worldedit/model/wand/Direction.java
@@ -5,8 +5,8 @@ import com.mbserver.api.game.Location;
 public enum Direction {
     UP( 0, 1, 0 ),
     DOWN( 0, -1, 0 ),
-    EAST( -1, 0, 0 ),
-    WEST( 1, 0, 0 ),
+    EAST( 1, 0, 0 ),
+    WEST( -1, 0, 0 ),
     NORTH( 0, 0, -1 ),
     SOUTH( 0, 0, 1 );
 
