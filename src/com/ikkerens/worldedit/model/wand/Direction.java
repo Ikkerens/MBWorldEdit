0c871a854e28abb1722a5e95eb8ac362975dc033
ikkerens <ikkerens@space-walrus.com>
2014-11-19 14:29:50 +0100

    Updated directions

--- src/com/ikkerens/worldedit/model/wand/Direction.java
+++ src/com/ikkerens/worldedit/model/wand/Direction.java
@@ -5,10 +5,10 @@ import com.mbserver.api.game.Location;
 public enum Direction {
     UP( 0, 1, 0 ),
     DOWN( 0, -1, 0 ),
-    EAST( 1, 0, 0 ),
-    WEST( -1, 0, 0 ),
-    NORTH( 0, 0, 1 ),
-    SOUTH( 0, 0, -1 );
+    EAST( -1, 0, 0 ),
+    WEST( 1, 0, 0 ),
+    NORTH( 0, 0, -1 ),
+    SOUTH( 0, 0, 1 );
 
     private int x, y, z;
 
