1022bdb1e4301026bfd29e0cb8d44a77845b87bd
ikkerens <ikkerens@space-walrus.com>
2014-11-24 16:21:38 +0100

    Fixed //pos creating weird selection shapes

--- src/com/ikkerens/worldedit/commands/PositionCommand.java
+++ src/com/ikkerens/worldedit/commands/PositionCommand.java
@@ -1,10 +1,12 @@
 package com.ikkerens.worldedit.commands;
 
+import com.ikkerens.worldedit.Util;
 import com.ikkerens.worldedit.WorldEditPlugin;
 import com.ikkerens.worldedit.handlers.AbstractCommand;
 import com.ikkerens.worldedit.model.Selection;
 import com.ikkerens.worldedit.model.events.SelectionCommandEvent;
 
+import com.mbserver.api.game.Location;
 import com.mbserver.api.game.Player;
 
 public class PositionCommand extends AbstractCommand< WorldEditPlugin > {
@@ -21,11 +23,14 @@ public class PositionCommand extends AbstractCommand< WorldEditPlugin > {
         final PositionCommandEvent event = new PositionCommandEvent( player, label.equalsIgnoreCase( "/pos1" ) );
         this.getPlugin().getPluginManager().triggerEvent( event );
 
-        if ( !event.isCancelled() )
+        if ( !event.isCancelled() ) {
+            final Location pLoc = player.getLocation();
+            final Location roundedLocation = Util.newLocation( pLoc.getWorld(), pLoc.getBlockX(), pLoc.getBlockY(), pLoc.getBlockZ() );
             if ( label.equalsIgnoreCase( "/pos1" ) )
-                sel.setPosition1( player.getLocation() );
+                sel.setPosition1( roundedLocation );
             else
-                sel.setPosition2( player.getLocation() );
+                sel.setPosition2( roundedLocation );
+        }
     }
 
     public static class PositionCommandEvent extends SelectionCommandEvent {
